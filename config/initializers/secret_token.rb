77fd879f187dfcec1eac8a9e5d966a0207cca165
Sanborn <sanbornh@rogers.com>
2014-05-28 21:23:58 -0400

    Setup new rails app. Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PortfolioSite::Application.config.secret_key_base = '0c3d2b23f95484b4650c782c5f75fe19da3f625ceec5a84100b89932c5d2954a761941c3ea48a06a220beb418b685c6f06266045e0bf6f3f821ef4a65db3a0ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PortfolioSite::Application.config.session_store :cookie_store, key: '_portfolio_site_session'
