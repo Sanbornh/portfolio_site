3027706a0ae3013fa0bfe01a58ef1b3b84d61fa6
Sanborn <sanbornh@rogers.com>
2014-05-31 14:59:33 -0400

    Updated layout and sorted out social links

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,6 +15,14 @@ function footerShrink() {
 		
 };
 
+function socialGrow() {
+	//stuff
+}
+
+function socialShrink() {
+	//stuff
+}
+
 // ----- ACTUAL JS SHIT ----- \\
 
 $( window ).ready(function() {
@@ -26,5 +34,21 @@ $( window ).ready(function() {
 	$('.content-container').mouseleave(function() {
 		footerShrink();
 	});
+
+	$(window).scroll(function() {
+	  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $('.social-links').animate({top: '-5%'}, {duration: 200, easing: 'easeOutQuad'});
+
+      setTimeout(function() {
+      	$('.social-links').css('z-index', 1);
+      }, 200);
+	  }
+
+	  if($(window).scrollTop() == 0) {
+	  	 $('.social-links').animate({top: '0%'}, {duration: 200, easing: 'easeOutQuad'}).css('z-index', -1);	
+	  }
+	});
+
+
 
 });	
